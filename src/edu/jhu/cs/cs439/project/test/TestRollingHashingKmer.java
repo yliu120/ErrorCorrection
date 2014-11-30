48b5f8d92189f23cca7d68284a32a5523cf12b62
Yunlong Liu <davislong198833@gmail.com>
2014-11-30 17:13:35 -0500

    Tested RollingHashingKMers

--- src/edu/jhu/cs/cs439/project/test/TestRollingHashingKmer.java
+++ src/edu/jhu/cs/cs439/project/test/TestRollingHashingKmer.java
@@ -3,6 +3,9 @@
  */
 package edu.jhu.cs.cs439.project.test;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import junit.framework.TestCase;
 
 import org.junit.Before;
@@ -12,23 +15,33 @@ import edu.jhu.cs.cs439.project.RollingHashingKMers;
 
 /**
  * This class is for testing the Rolling hashing functions
+ * 
  * @author Yunlong Liu
- * @author Yijie   Li
+ * @author Yijie Li
  * @version 0.0
  * @since 0.0
  */
 public class TestRollingHashingKmer extends TestCase {
-	
+
 	private RollingHashingKMers hashing;
-	
+
 	@Before
 	public void setUp() {
 		hashing = new RollingHashingKMers(3, 15);
 	}
-	
+
 	@Test
 	public void testRollingHashingKmer() {
-		
+
+		String KMer_1 = "ACGTTGC";
+		String KMer_2 = "ACGTTGC";
+ 		for (int i = 0; i < 3; i++) {
+			int hashCode_1 = hashing.hashing(KMer_1, i);
+			int hashCode_2 = hashing.hashing(KMer_2, i);
+			assertEquals( ( hashCode_1 >= 0 && hashCode_1 <= 14 ) , true);
+			assertEquals( ( hashCode_1 >= 0 && hashCode_1 <= 14 ) , true);
+			assertEquals( hashCode_1 , hashCode_2 );
+		}
 	}
 
 }
