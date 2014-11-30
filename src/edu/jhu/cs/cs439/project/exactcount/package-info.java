b00e373114b2f5015a9bb81ae32bad7d10506fd0
Yunlong Liu <davislong198833@gmail.com>
2014-11-29 20:54:28 -0500

    Build up the basic packages

---
+++ src/edu/jhu/cs/cs439/project/exactcount/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package implements the exactcount by using Apache Spark - MapReduce
+ * @author Yunlong Liu
+ * @author Yijie   Li
+ * @version 0.0
+ */
+package edu.jhu.cs.cs439.project.exactcount;
---
+++ src/edu/jhu/cs/cs439/project/kmercountwithcmsketch/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * This package implements the counting k-mers algorithm using
+ * spark 1.1 based on the count-min sketch data structure. 
+ * @author Yunlong Liu
+ * @author Yijie   Li
+ * @version 1.0
+ */
+package edu.jhu.cs.cs439.project.kmercountwithcmsketch;
